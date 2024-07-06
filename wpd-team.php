e4c7d41d4d93b4d8c301e2f9fbc1313c4f9c7bb8
Robiul Islam <therobiulislam12@gmail.com>
2024-07-07 00:05:31 +0600

    Initial Commit

---
+++ wpd-team.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Plugin Name: WDA Team
+ * Description: Create a custom post type and add some functionality
+ * Plugin URI: #
+ * Version: 1.0.0
+ * Author: Robiul Islam
+ * Author URI: https://robiul-islam.netlify.app
+ */
