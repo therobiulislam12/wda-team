4f4288384620f4615a4f28ad3ce303ea77ad0a8f
Robiul Islam <therobiulislam12@gmail.com>
2024-07-07 00:16:41 +0600

    create instance for main class

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Uninstall file
+ * 
+ * @since 1.0.0
+ */
+
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
--- wpd-team.php
+++ wpd-team.php
@@ -7,3 +7,42 @@
  * Author: Robiul Islam
  * Author URI: https://robiul-islam.netlify.app
  */
+
+// if ABSPATH is not define, exit
+if ( ! defined( 'ABSPATH' ) ) {
+    exit();
+}
+
+/**
+ * Create a final class for our main plugin
+ */
+final class Wpd_Team{
+    // declare instance
+    public static $instance;
+
+    /**
+     * Create a getInstance method for our class
+     * 
+     * @return \Wpd_Team
+     * @since 1.0.0
+     */
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
+
+/**
+ * create a function for call the Wpd_Team Class
+ *      
+ * @return \Wpd_Team
+ */
+function wpd_team(){
+    return Wpd_Team::getInstance();
+}
+
+// kick of the function
+wpd_team();
