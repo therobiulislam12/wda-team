a455dd53ccd47b41c41fab60e31b6df8551b4ade
Robiul Islam <therobiulislam12@gmail.com>
2024-07-08 03:25:25 +0600

    Vendor file added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'therobiulislam12/wpd-team',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '04f5273e871b336d54761bf64bedb5e247cb842e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'therobiulislam12/wpd-team' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '04f5273e871b336d54761bf64bedb5e247cb842e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
